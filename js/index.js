1fd4ebdeabc54f6f267c40c3d89c24bc0ba29154
mihguelsilva <mihguelaraujo@gmail.com>
2023-03-16 06:44:31 -0300

    Melhorando representação dos ícones entre operações distintas

--- js/index.js
+++ js/index.js
@@ -1,30 +1,53 @@
 /*
   OPERAÇÕES DE SOMA
 */
-var soma = [
-    document.querySelector("#number1"),
-    document.querySelector("#number2"),
-    document.querySelector("#id-div-resposta-soma")
-]
-var icon = [
-    document.querySelector("#icon-number1"),
-    document.querySelector("#icon-number2"),
-    document.querySelector("#icon-result")
-]
 
+// Resetar valores
+document.querySelectorAll(".form-control").forEach((value, index) => {
+    document.querySelectorAll(".form-control")[index].value = 0;
+});
+
+var number1 = 0;
+var number2 = 0;
+
+var p = document.querySelector("#id-div-resposta-soma");
+
+var icon = [
+    document.querySelector("#icon-number1-soma"),
+    document.querySelector("#icon-number2-soma"),
+    document.querySelector("#icon-result-soma")
+]
+var ops = "soma";
 var imagem = "../img/operacoes/maca.png"
 
-soma[0].value = "";
-soma[1].value = "";
+function resetNum() {
+    number1 = 0;
+    number2 = 0;
+}
 
-function result(ops) {
+function change(ops) {
+    p = document.querySelector("#id-div-resposta-"+ops)
+    icon[0] = document.querySelector("#icon-number1-"+ops);
+    icon[1] = document.querySelector("#icon-number2-"+ops);
+    icon[2] = document.querySelector("#icon-result-"+ops);
+}
+
+function result(ops, number1, number2) {
+    console.log(number1);
+    console.log(number2)
+    let resultado = ""
+
     // Realizando calculo
     if (ops == "soma") {
-        let number1 = Number(soma[0].value);
-        let number2 = Number(soma[1].value);
-        let resultado = number1 + number2
-        soma[2].innerHTML = resultado;
+        resultado = number1 + number2
+    } else if (ops == "sub") {
+        resultado = number1 - number2
+    } else if (ops == "mult") {
+        resultado = number1 * number2
+    } else if (ops == "div") {
+        resultado = number1 / number2
     }
+    p.innerHTML = resultado;
 
     // Resetando representação do resultado final por ícone
     var img = document.querySelectorAll(".icon-result");
@@ -34,7 +57,7 @@ function result(ops) {
 
     // Representação gráfica do resultado final
     var divResult = []
-    for (let a = 0; a < Number(soma[2].innerHTML); a++) {
+    for (let a = 0; a < Number(resultado); a++) {
         divResult.push(document.createElement("div"))
     }
     for (let a = 0; a < divResult.length; a++) {
@@ -43,7 +66,7 @@ function result(ops) {
     }
 }
 
-function graphNum1() {
+function graphNum1(number1) {
 
     // Resetando representação gráfica
     let img = document.querySelectorAll(".icon-number-1");
@@ -52,9 +75,9 @@ function graphNum1() {
     }
 
     // Representação gráfica
-    if (soma[0].value > 0) {
+    if (number1 > 0) {
         var divNum1 = []
-        for (let a = 0; a < soma[0].value; a++) {
+        for (let a = 0; a < number1; a++) {
             divNum1.push(document.createElement("div"))
         }
         for (let a = 0; a < divNum1.length; a ++) {
@@ -64,7 +87,7 @@ function graphNum1() {
     }
 }
 
-function graphNum2() {
+function graphNum2(number2) {
 
     // Resetando representação gráfica
     let img = document.querySelectorAll(".icon-number-2");
@@ -73,9 +96,9 @@ function graphNum2() {
     }
 
     // Representação gráfica
-    if (soma[1].value > 0) {
+    if (number2 > 0) {
         var divNum2 = []
-        for (let a = 0; a < soma[1].value; a++) {
+        for (let a = 0; a < number2; a++) {
             divNum2.push(document.createElement("div"))
         }
         for (let a = 0; a < divNum2.length; a ++) {
@@ -84,46 +107,39 @@ function graphNum2() {
         }
     }
 }
-soma[0].addEventListener("keyup", (n) => {
-    result("soma")
-    graphNum1()
-});
-soma[1].addEventListener("keyup", (n) => {
-    result("soma")
-    graphNum2()
-});
-soma[0].addEventListener("change", (n) => {
-    result("soma")
-    graphNum1()
-});
-soma[1].addEventListener("change", (n) => {
-    result("soma")
-    graphNum2()
+
+document.querySelectorAll(".form-control").forEach((value, index) => {
+    document.querySelectorAll(".form-control")[index].addEventListener("keyup", (e) => {
+        if(e.target.name == "num1") {
+            number1 = Number(e.target.value);
+        } else if (e.target.name == "num2") {
+            number2 = Number(e.target.value);
+        }
+        result(ops, number1, number2)
+        graphNum1(number1)
+        graphNum2(number2)
+    })
+})
+
+document.querySelectorAll(".ops").forEach((value, index) => {
+    document.querySelectorAll(".ops")[index].addEventListener("click", (e) => {
+        console.log(e.target.name)
+        if (e.target.name == "soma") {
+            resetNum()
+            ops = "soma";
+            change(ops);
+        } else if (e.target.name == "sub") {
+            resetNum()
+            ops = "sub";
+            change(ops);
+        } else if (e.target.name == "div") {
+            ops = "div";
+        } else if (e.target.name == "mult") {
+            ops = "mult";
+        }
+    });
 });
 
 /*
   OPERAÇÕES DE SUBTRAÇÃO
 */
-
-var sub = [
-    document.querySelector("#number1"),
-    document.querySelector("#number2"),
-    document.querySelector("#id-div-resposta-soma")
-]
-
-sub[0].addEventListener("keyup", (n) => {
-    result("soma")
-    graphNum1()
-});
-sub[1].addEventListener("keyup", (n) => {
-    result("soma")
-    graphNum2()
-});
-sub[0].addEventListener("change", (n) => {
-    result("soma")
-    graphNum1()
-});
-sub[1].addEventListener("change", (n) => {
-    result("soma")
-    graphNum2()
-});
